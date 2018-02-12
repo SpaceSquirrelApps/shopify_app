bfb125eb5cb0eb20aeb0854b0f89c1ed5ef81b94
Domagoj Barisic <domagoj.barisic@gmail.com>
2018-02-12 18:15:19 +0000

    Fix safari issue

--- app/controllers/shopify_app/authenticated_controller.rb
+++ app/controllers/shopify_app/authenticated_controller.rb
@@ -7,5 +7,19 @@ module ShopifyApp
     protect_from_forgery with: :exception
     before_action :login_again_if_different_shop
     around_action :shopify_session
+
+    protected
+      def redirect_to_login
+        if request.xhr?
+          head :unauthorized
+        else
+          session[:return_to] = request.fullpath if request.get?
+          if browser.safari?
+            fullpage_redirect_to login_url
+          else
+            redirect_to login_url
+          end
+        end
+      end
   end
 end
